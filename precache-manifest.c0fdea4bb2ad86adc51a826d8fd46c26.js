96b81b4c6c774b2bdf10b9e278dfe5979b9e7060
MarMMcin <em-erce@wp.pl>
2019-09-16 23:52:00 +0200

    Updates

---
+++ precache-manifest.c0fdea4bb2ad86adc51a826d8fd46c26.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c6f6f41e061adcea24a47d86885ecb0",
+    "url": "/weatherapp/index.html"
+  },
+  {
+    "revision": "b2a8baee44ba077e8a1f",
+    "url": "/weatherapp/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "ce779b3b0185c6db0b98",
+    "url": "/weatherapp/static/css/main.2af33ad6.chunk.css"
+  },
+  {
+    "revision": "b2a8baee44ba077e8a1f",
+    "url": "/weatherapp/static/js/2.761d2eed.chunk.js"
+  },
+  {
+    "revision": "ce779b3b0185c6db0b98",
+    "url": "/weatherapp/static/js/main.dfa61efd.chunk.js"
+  },
+  {
+    "revision": "7c6724536b514252bb98",
+    "url": "/weatherapp/static/js/runtime~main.bb086d8a.js"
+  }
+]);
